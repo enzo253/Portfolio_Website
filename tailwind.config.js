56dcd79758b865c43d4f5041fd7ee6f6924080ee
enzo253 <139024939+enzo253@users.noreply.github.com>
2025-04-07 21:33:18 +0200

    started website added tailwind for css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './projects.html',
+    './about.html',
+    './contact.html',
+    './assets/style.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
